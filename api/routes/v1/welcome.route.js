c63bf130088e77bbaa81f4a73f4c3035e8d53ee9
Omar Dulaimi <omar@eila.io>
2019-07-05 21:23:49 +0300

    refactor welcome route to use it's controller

--- api/routes/v1/welcome.route.js
+++ api/routes/v1/welcome.route.js
@@ -1,7 +1,5 @@
-module.exports = async (req, res, method) => {
-    res.writeHead(200, { 'Content-Type': 'text/plain' });
-    res.write(
-        'Welcome to our pure NodeJS Api. Please use /note to do your development.',
-    );
-    res.end();
+const controller = require('../../controllers/welcome.controller');
+
+module.exports = async (req, res) => {
+    controller.get(req, res);
 };
