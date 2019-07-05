a8ce24a7f204606b733f8be0f7d6a8721411182b
Omar Dulaimi <omar@eila.io>
2019-07-05 21:50:57 +0300

    apply exception handling to error prone parts

--- api/controllers/note.controller.js
+++ api/controllers/note.controller.js
@@ -31,13 +31,17 @@ exports.post = (req, res) => {
     });
 
     req.on('end', async () => {
-        const parsedBody = Buffer.concat(body).toString();
-        const parsedNote = JSON.parse(parsedBody);
-        const note = await Note.create({
-            title: parsedNote.title,
-            content: parsedNote.content,
-        });
-        res.end(JSON.stringify(note));
+        try {
+            const parsedBody = Buffer.concat(body).toString();
+            const parsedNote = JSON.parse(parsedBody);
+            const note = await Note.create({
+                title: parsedNote.title,
+                content: parsedNote.content,
+            });
+            res.end(JSON.stringify(note));
+        } catch (e) {
+            res.end(JSON.stringify(e));
+        }
     });
 };
 
@@ -53,21 +57,25 @@ exports.put = (req, res) => {
 
     const body = [];
     const parsed = url.parse(req.url);
-    const query = querystring.parse(parsed.query);
+    const query = querystring.parse(parsed.query); // You get the param Id here if you want to
 
     req.on('data', (chunk) => {
         body.push(chunk);
     });
 
     req.on('end', async () => {
-        const parsedBody = Buffer.concat(body).toString();
-        const parsedNote = JSON.parse(parsedBody);
-        const updatedNote = await Note.findById(parsedNote._id);
-        updatedNote.title = parsedNote.title;
-        updatedNote.content = parsedNote.content;
-        await updatedNote.save();
+        try {
+            const parsedBody = Buffer.concat(body).toString();
+            const parsedNote = JSON.parse(parsedBody);
+            const updatedNote = await Note.findById(parsedNote._id);
+            updatedNote.title = parsedNote.title;
+            updatedNote.content = parsedNote.content;
+            await updatedNote.save();
 
-        res.end(JSON.stringify(updatedNote));
+            res.end(JSON.stringify(updatedNote));
+        } catch (e) {
+            res.end(JSON.stringify(e));
+        }
     });
 };
 
@@ -90,12 +98,15 @@ exports.delete = (req, res) => {
     });
 
     req.on('end', async () => {
-        const parsedBody = Buffer.concat(body).toString();
-        const parsedNote = JSON.parse(parsedBody);
-
-        const updatedNote = await Note.findById(parsedNote._id);
-        updatedNote.isDeleted = true;
-        await updatedNote.save();
-        res.end(JSON.stringify(updatedNote));
+        try {
+            const parsedBody = Buffer.concat(body).toString();
+            const parsedNote = JSON.parse(parsedBody);
+            const updatedNote = await Note.findById(parsedNote._id);
+            updatedNote.isDeleted = true;
+            await updatedNote.save();
+            res.end(JSON.stringify(updatedNote));
+        } catch (e) {
+            res.end(JSON.stringify(e));
+        }
     });
 };
